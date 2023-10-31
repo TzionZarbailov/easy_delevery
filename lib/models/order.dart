54bbbb8b90c6c41c5f6ee517d961b2bf63108174
TzionK3007 <133116875+TzionZarbailov@users.noreply.github.com>
2023-10-31 14:02:14 +0200

    Update order.dart

--- lib/models/order.dart
+++ lib/models/order.dart
@@ -1,13 +1,13 @@
 class Order {
   final String id;
-  final String consumerId;
-  final String restaurantId;
-  final String menuItemId;
-  final int quantity;
-  final double totalPrice;
-  final String orderStatus;
-  final DateTime orderTime;
-  final bool pickUpOrDelivery;
+  final String consumerId; //*This is the id of the consumer who placed the order
+  final String restaurantId; //*This is the id of the restaurant where the order is placed
+  final String menuItemId; //*This is the id of the menu item ordered
+  final int quantity; //*This is the quantity of the item ordered
+  final double totalPrice; //*This is the total price of the order
+  final String orderStatus; //*This is the status of the order
+  final DateTime orderTime; //*This is the time when the order is placed
+  final bool pickUpOrDelivery; //*If it is false then it is TA if it is true then it is delivery
 
   const Order({
     required this.id,
