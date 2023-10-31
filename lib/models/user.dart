6e02da8cc55394d6a0d938e96c9a7b81bffe6ef9
TzionK3007 <133116875+TzionZarbailov@users.noreply.github.com>
2023-10-31 13:42:50 +0200

    Create user.dart

---
+++ lib/models/user.dart
@@ -0,0 +1,19 @@
+class User {
+  final String id;
+  final String name;
+  final String email;
+  final String password;
+  final String userType;
+  final String address;
+  final String phoneNumber;
+
+  const User({
+    required this.id,
+    required this.name,
+    required this.email,
+    required this.password,
+    required this.userType,
+    required this.address,
+    required this.phoneNumber,
+  });
+}
