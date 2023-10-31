04b60b35ef897b36aa5b227876b108d1ddbcda35
TzionK3007 <133116875+TzionZarbailov@users.noreply.github.com>
2023-10-31 13:43:09 +0200

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -15,6 +15,7 @@ class MyApp extends StatelessWidget {
       debugShowCheckedModeBanner: false,
       theme: ThemeData(
         useMaterial3: true,
+        
       ),
       home: const HomeScreen(),
     );
