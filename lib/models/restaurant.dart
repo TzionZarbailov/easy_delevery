1badeffd9a696f98ca533041b57620b3e0a8f020
TzionK3007 <133116875+TzionZarbailov@users.noreply.github.com>
2023-10-31 14:02:17 +0200

    Update restaurant.dart

--- lib/models/restaurant.dart
+++ lib/models/restaurant.dart
@@ -2,12 +2,12 @@ import 'package:easy_delevery/models/category.dart';
 import 'package:easy_delevery/models/menu_item.dart';
 
 class Restaurant {
-  final String id;
-  final String name;
-  final String address;
-  final bool isOpen;
-  final List<Category> categories;
-  final List<MenuItem> menuItems;
+  final String id; //*This is the id of the restaurant
+  final String name; //*This is the name of the restaurant
+  final String address; //*This is the address of the restaurant
+  final bool isOpen; //*This is the status of the restaurant
+  final List<Category> categories; //*This is the list of categories of the restaurant
+  final List<MenuItem> menuItems; //*This is the list of menu items of the restaurant
 
   const Restaurant({
     required this.id,
