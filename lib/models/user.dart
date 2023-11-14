7a7a0cf2338ef58910e840277b9aba416a08c778
TzionK3007 <133116875+TzionZarbailov@users.noreply.github.com>
2023-11-14 13:57:56 +0200

    Update user.dart

--- lib/models/user.dart
+++ lib/models/user.dart
@@ -1,11 +1,11 @@
 class User {
-  final String id; //*This is the id of the user
-  final String name; //*This is the name of the user
-  final String email; //*This is the email of the user
-  final String password; //*This is the password of the user
-  final String userType; //*This is the type of the user
-  final String address; //*This is the address of the user
-  final String phoneNumber; //*This is the phone number of the user
+  final String id; //* This is the id of the user
+  final String name; //* This is the name of the user
+  final String email; //* This is the email of the user
+  final String password; //* This is the password of the user
+  final String userType; //* This is the type of the user
+  final String address; //* This is the address of the user
+  final String phoneNumber; //* This is the phone number of the user
 
   const User({
     required this.id,
