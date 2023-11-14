28140cda585c8ab3c81d252d33f56db1e1e4aef3
TzionK3007 <133116875+TzionZarbailov@users.noreply.github.com>
2023-11-14 13:57:37 +0200

    Create my_colors.dart

---
+++ lib/colors/my_colors.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+// ignore: camel_case_types
+class myColors {
+  static const Color inputColor = Color(0xFFF98F13);
+  static const Color buttonColor = Color(0xFFF98F13);
+  static Color focuseInputColor = Colors.orange.shade300;
+  static const Color backgroundColor = Colors.black;
+  static const Color textColor = Colors.white;
+}
