3c19c7934410077206492e051fd28cfe87d45312
TzionK3007 <133116875+TzionZarbailov@users.noreply.github.com>
2023-11-14 16:15:09 +0200

    Create helper_function.dart

---
+++ lib/helper/helper_function.dart
@@ -0,0 +1,33 @@
+// ignore: unused_import
+import 'package:flutter/material.dart';
+
+//! Password correctness
+bool isPasswordCorrect() {
+  return true;
+}
+
+//! Email correctness
+bool isEmailCorrect() {
+  return true;
+}
+
+//! login functions
+void login() {}
+
+//! login with google
+void loginWithGoogle() {}
+
+//! login with facebook
+void loginWithFacebook() {}
+
+//! reset password
+void resetPassword() {}
+
+//! reset password with email
+void resetPasswordWithEmail() {}
+
+//! reset password with phone number
+void resetPasswordWithPhone() {}
+
+//! reset password with email or phone number
+void resetPasswordWithEmailOrPhone() {}
