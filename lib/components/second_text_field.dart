e1682255a38055b1a3252877e6310365cdf73189
TzionK3007 <133116875+TzionZarbailov@users.noreply.github.com>
2023-11-14 13:57:47 +0200

    Create second_text_field.dart

---
+++ lib/components/second_text_field.dart
@@ -0,0 +1,55 @@
+import 'package:easy_delevery/colors/my_colors.dart';
+import 'package:flutter/material.dart';
+
+class SecondTextField extends StatelessWidget {
+  final TextEditingController controller;
+  final String labelText;
+  final bool obscureText;
+  final TextInputType keyboardType;
+  final void Function() onTap;
+
+  const SecondTextField({
+    super.key,
+    required this.onTap,
+    required this.keyboardType,
+    required this.controller,
+    required this.labelText,
+    required this.obscureText,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return TextField(
+      onTap: onTap,
+      keyboardType: keyboardType,
+      obscureText: obscureText,
+      style: const TextStyle(
+        color: Colors.white,
+      ),
+      textAlign: TextAlign.center,
+      controller: controller,
+      cursorColor: myColors.inputColor,
+      decoration: InputDecoration(
+        label: Row(
+          mainAxisAlignment: MainAxisAlignment.end,
+          children: [
+            Text(
+              labelText,
+              style: const TextStyle(
+                color: myColors.textColor,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+          ],
+        ),
+        enabledBorder: const UnderlineInputBorder(
+          borderSide: BorderSide(color: myColors.inputColor),
+        ),
+        focusedBorder: UnderlineInputBorder(
+          borderSide: BorderSide(color: myColors.focuseInputColor),
+          borderRadius: BorderRadius.circular(8),
+        ),
+      ),
+    );
+  }
+}
