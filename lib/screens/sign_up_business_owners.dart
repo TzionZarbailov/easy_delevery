004e1926d2b7b67832643177995c2f5d2576ad27
TzionK3007 <133116875+TzionZarbailov@users.noreply.github.com>
2023-11-14 13:58:17 +0200

    Update sign_up_business_owners.dart

--- lib/screens/sign_up_business_owners.dart
+++ lib/screens/sign_up_business_owners.dart
@@ -1,12 +1,297 @@
+import 'package:easy_delevery/components/my_button.dart';
+import 'package:easy_delevery/components/second_text_field.dart';
+import 'package:easy_delevery/components/text_home_screen.dart';
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
-class SignUpBusinessOwners extends StatelessWidget {
+class SignUpBusinessOwners extends StatefulWidget {
   const SignUpBusinessOwners({super.key});
 
   @override
+  State<SignUpBusinessOwners> createState() => _SignUpBusinessOwnersState();
+}
+
+class _SignUpBusinessOwnersState extends State<SignUpBusinessOwners> {
+  //* text controllers
+  final TextEditingController _emailControler = TextEditingController();
+  final TextEditingController _nameController = TextEditingController();
+  final TextEditingController _phoneController = TextEditingController();
+  final TextEditingController _restaurantPhoneController =
+      TextEditingController();
+  final TextEditingController _passwordController = TextEditingController();
+  final TextEditingController _restaurantNameController =
+      TextEditingController();
+  final TextEditingController _streetController = TextEditingController();
+  final TextEditingController _timeController = TextEditingController();
+
+  @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      backgroundColor: Colors.green,
+    //* height: MediaQuery.of(context).size.height,
+    double height = MediaQuery.of(context).size.height;
+
+    //* width: MediaQuery.of(context).size.width,
+    double width = MediaQuery.of(context).size.width;
+
+    return Scaffold(
+      backgroundColor: Colors.black,
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            Stack(
+              children: [
+                //* image customer_registration
+                Image.asset(
+                  'lib/assets/images/business_registration_image.jpeg',
+                  height: height / 1.6,
+                  width: width,
+                  fit: BoxFit.cover,
+                ),
+                SafeArea(
+                  child: Padding(
+                    padding: const EdgeInsets.symmetric(
+                        horizontal: 30, vertical: 40),
+                    child: Column(
+                      children: [
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.end,
+                          children: const [
+                            Text(
+                              'ברוכים הבאים',
+                              style: TextStyle(
+                                color: Colors.white,
+                                fontSize: 40,
+                                fontWeight: FontWeight.w400,
+                              ),
+                            )
+                          ],
+                        ),
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.end,
+                          children: [
+                            Text(
+                              'Easy_Delevery',
+                              style: GoogleFonts.abrilFatface(
+                                color: const Color(0xFFF98F13),
+                                fontSize: 40,
+                                fontWeight: FontWeight.w500,
+                                letterSpacing: 2,
+                              ),
+                            ),
+                            const Text(
+                              '-ל',
+                              style: TextStyle(
+                                color: Colors.white,
+                                fontSize: 40,
+                                fontWeight: FontWeight.w400,
+                              ),
+                            ),
+                          ],
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+                //* Icon button to go back
+                SafeArea(
+                  child: Padding(
+                    padding: const EdgeInsets.symmetric(horizontal: 8),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.start,
+                      children: [
+                        IconButton(
+                          icon: const Icon(
+                            Icons.arrow_back_ios_new_rounded,
+                            color: Color(0xFFF98F13),
+                            size: 26.5,
+                          ),
+                          onPressed: () {
+                            Navigator.pop(context);
+                          },
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+                Positioned(
+                  bottom: 0,
+                  left: 0,
+                  right: 0,
+                  child: Container(
+                    decoration: const ShapeDecoration(
+                      gradient: LinearGradient(
+                        begin: Alignment.topCenter,
+                        end: Alignment.bottomCenter,
+                        colors: [
+                          Color(0x00181A26),
+                          Color.fromARGB(217, 0, 0, 0),
+                        ],
+                      ),
+                      shape: RoundedRectangleBorder(),
+                    ),
+                    child: Column(
+                      children: const [
+                        TextHomeScreen(
+                          text: 'Easy_Delevery-הצטרפות ל',
+                          fontSize: 20,
+                        ),
+                        TextHomeScreen(
+                          text: 'אפליקצית משלוחי האוכל הגדולה בישראל',
+                          fontSize: 20,
+                        ),
+                        TextHomeScreen(
+                          text: 'הצטרפו עכשיו והגדילו את פעילות',
+                          fontSize: 20,
+                        ),
+                        TextHomeScreen(
+                          text: 'המשלוחים וההכנסות שלכם',
+                          fontSize: 20,
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+              ],
+            ),
+            Container(
+              padding: const EdgeInsets.symmetric(horizontal: 15),
+              child: Column(
+                children: [
+                  Row(
+                    children: [
+                      Expanded(
+                        child: Padding(
+                          padding: EdgeInsets.only(left: width / 3),
+                          child: SecondTextField(
+                            onTap: () {},
+                            keyboardType: TextInputType.emailAddress,
+                            controller: _emailControler,
+                            labelText: 'דוא"ל',
+                            obscureText: false,
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                  Row(
+                    children: [
+                      Expanded(
+                        child: Padding(
+                          padding: EdgeInsets.only(left: width / 3),
+                          child: SecondTextField(
+                            onTap: () {},
+                            keyboardType: TextInputType.emailAddress,
+                            controller: _passwordController,
+                            labelText: 'סיסמה',
+                            obscureText: true,
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                  Row(
+                    children: [
+                      Expanded(
+                        child: Padding(
+                          padding: EdgeInsets.only(left: width / 15),
+                          child: SecondTextField(
+                            onTap: () {},
+                            keyboardType: TextInputType.number,
+                            controller: _phoneController,
+                            labelText: 'טלפון',
+                            obscureText: false,
+                          ),
+                        ),
+                      ),
+                      const SizedBox(width: 40),
+                      Expanded(
+                        child: SecondTextField(
+                          onTap: () {},
+                          keyboardType: TextInputType.text,
+                          controller: _nameController,
+                          labelText: 'שם מלא',
+                          obscureText: false,
+                        ),
+                      ),
+                    ],
+                  ),
+                  Row(
+                    children: [
+                      Expanded(
+                        child: Padding(
+                          padding: EdgeInsets.only(left: width / 3),
+                          child: SecondTextField(
+                            onTap: () {},
+                            keyboardType: TextInputType.text,
+                            controller: _restaurantNameController,
+                            labelText: 'שם המסעדה',
+                            obscureText: false,
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                  Row(
+                    children: [
+                      Expanded(
+                        child: Padding(
+                          padding: EdgeInsets.only(left: width / 15),
+                          child: SecondTextField(
+                            onTap: () {},
+                            keyboardType: TextInputType.streetAddress,
+                            controller: _streetController,
+                            labelText: 'עיר',
+                            obscureText: false,
+                          ),
+                        ),
+                      ),
+                      const SizedBox(width: 40),
+                      Expanded(
+                        child: SecondTextField(
+                          onTap: () {},
+                          keyboardType: TextInputType.number,
+                          controller: _restaurantPhoneController,
+                          labelText: 'טלפון של מסעדה',
+                          obscureText: false,
+                        ),
+                      ),
+                    ],
+                  ),
+                  Row(
+                    children: [
+                      Expanded(
+                        child: Padding(
+                          padding: EdgeInsets.only(left: width / 2),
+                          child: SecondTextField(
+                            onTap: () {},
+                            keyboardType: TextInputType.datetime,
+                            controller: _timeController,
+                            labelText: 'שעות פעילות',
+                            obscureText: false,
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.symmetric(horizontal: 15),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.start,
+                      children: [
+                        MyButton(
+                          text: 'הרשמה',
+                          horizontal: 25,
+                          vertical: double.minPositive,
+                          onTap: () {},
+                        ),
+                      ],
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
