a8fe2bcaca29ffcac4b781aa2ad2204da8f40f98
TzionK3007 <133116875+TzionZarbailov@users.noreply.github.com>
2023-11-14 16:15:18 +0200

    Create business_owner_home_screen.dart

---
+++ lib/screens/business_owner_home_screen.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class BusinessOwnerHomeScreen extends StatefulWidget {
+  const BusinessOwnerHomeScreen({super.key});
+
+  @override
+  State<BusinessOwnerHomeScreen> createState() => _BusinessOwnerHomeScreenState();
+}
+
+class _BusinessOwnerHomeScreenState extends State<BusinessOwnerHomeScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      backgroundColor: Colors.greenAccent,
+    );
+  }
+}
