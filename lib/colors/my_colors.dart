667a85e0fdc728bd88d2e5728e43e7ae09da91cf
TzionK3007 <133116875+TzionZarbailov@users.noreply.github.com>
2023-11-14 16:14:59 +0200

    Update my_colors.dart

--- lib/colors/my_colors.dart
+++ lib/colors/my_colors.dart
@@ -6,5 +6,7 @@ class myColors {
   static const Color buttonColor = Color(0xFFF98F13);
   static Color focuseInputColor = Colors.orange.shade300;
   static const Color backgroundColor = Colors.black;
+  static const Color errorMassage = Colors.red;
   static const Color textColor = Colors.white;
+  static Color greyColor = Colors.grey.shade600;
 }
