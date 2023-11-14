761300c591fe48ca61c0e0986c85553078d32ca3
TzionK3007 <133116875+TzionZarbailov@users.noreply.github.com>
2023-11-14 16:15:12 +0200

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,10 @@
+import 'package:easy_delevery/screens/business_owner_home_screen.dart';
+import 'package:easy_delevery/screens/consumer_home_screen.dart';
 import 'package:easy_delevery/screens/login_screen.dart';
+import 'package:easy_delevery/screens/registration_screen.dart';
+import 'package:easy_delevery/screens/reset_password.dart';
+import 'package:easy_delevery/screens/sign_up_business_owners.dart';
+import 'package:easy_delevery/screens/sign_up_customers.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 
@@ -22,6 +28,16 @@ class MyApp extends StatelessWidget {
       ),
       title: 'Easy_Delevery',
       home: const LoginScreen(),
+      routes: {
+        '/login_screen': (context) => const LoginScreen(),
+        '/registration_screen': (context) => const RegistrationScreen(),
+        '/reset_password': (context) => const ResetPassword(),
+        '/sign_up_business_owners': (context) => const SignUpBusinessOwners(),
+        '/sign_up_customers': (context) => const SignUpCustomers(),
+        '/business_owner_home_screen': (context) => const BusinessOwnerHomeScreen(),
+        '/consumer_home_screen': (context) => const ConsumerHomeScreen(),
+
+      },
     );
   }
 }
